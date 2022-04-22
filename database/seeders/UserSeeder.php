a8c2e51d61823ffa2bd579833022bbea4dda6629
Ahmad <ahmadabotoimah@gmail.com>
2022-04-22 23:20:09 +0300

    user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Ali',
+             'user_id' => '1859648',
+             'password' => Hash::make('password'),
+        ]);
+        User::create([
+            'name' => 'Hashemite',
+             'user_id' => '1256333333',
+             'password' => Hash::make('password'),
+             'role' => 'company',
+        ]);
+        User::create([
+            'name' => 'Alaa',
+             'user_id' => '1259874',
+             'password' => Hash::make('password'),
+             'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'Hani',
+             'user_id' => '156897',
+             'password' => Hash::make('password'),
+             'role' => 'supervisor',
+        ]);
     }
 }
